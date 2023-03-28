26909adea2270425052a0c1c241627d7d6c5f178
elly <omondyelly@gmail.com>
2023-03-28 16:32:44 +0300

    Added request API

---
+++ student_req/src/main/java/com/afwit/student_req/StudentReqApplication.java
@@ -0,0 +1,13 @@
+package com.afwit.student_req;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentReqApplication.class, args);
+	}
+
+}
---
+++ student_req/src/test/java/com/afwit/student_req/StudentReqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afwit.student_req;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentReqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
